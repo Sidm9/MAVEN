37514c8530082bb5e9d529e6e1f0a6a4112427d6
SidM9 <43111742+Sidm9@users.noreply.github.com>
2019-06-07 14:05:59 +0530

    REEDITED 5.)

--- src/main/java/in/ac/sharda/pizz_app/controller/CartController.java
+++ src/main/java/in/ac/sharda/pizz_app/controller/CartController.java
@@ -1,6 +1,8 @@
 package in.ac.sharda.pizz_app.controller;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.ac.sharda.pizz_app.domain.Cart;
@@ -11,10 +13,10 @@ import in.ac.sharda.pizz_app.domain.Product;
 
 public class CartController {
 	// 5.) ADDED A LIST OF CART HAVING PRODUCT
-	private List<Cart> products = new ArrayList<>();
+	private List<Product> products = new ArrayList<>();
 
-	public void addProduct(Cart cart) {
-		this.products.add(cart);
+	public void addProduct(Product product) {
+		this.products.add(product);
 	}
 	
 	
